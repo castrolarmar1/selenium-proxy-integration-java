a68da348603dee5a3eccfb7b4e49c542d8336b03
castrolarmar1 <princelarmar@gmail.com>
2025-08-03 03:56:09 +0100

    Update ProxySetup.java

--- src/main/java/ProxySetup.java
+++ src/main/java/ProxySetup.java
@@ -1,5 +1,5 @@
 final class ProxySetup {
     static final String ENDPOINT = "pr.oxylabs.io:7777";
-    static final String USERNAME = "";
-    static final String PASSWORD = "";
+    static final String castrolarmar1= "";
+    static final String onlineGUY1997+= "";
 }
